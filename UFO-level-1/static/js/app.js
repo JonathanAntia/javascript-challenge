3faeaa01db94c4412f69a9839c934377d5b1156c
JonathanAntia <a_jon12@yahoo.com>
2020-08-09 11:58:33 -0500

    javascript app loading data to html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// create a tbody variable to get a handle on the html element
+var tbody = d3.select('tbody');
+
+// write a function that will add data to the html table
+tableData.forEach(report => {
+    var row = tbody.append('tr');
+    Object.entries(report).forEach(([key,value]) => {
+        var cell = row.append('td');
+        cell.text(value);
+    })
+});
+
