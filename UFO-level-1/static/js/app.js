581c5d1e82a75cbc9adba459d4461994634afa8d
JonathanAntia <a_jon12@yahoo.com>
2020-08-15 15:56:39 -0500

    javascript app

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,23 +1,25 @@
 // from data.js
-var tableData = data;
+const tableData = data;
 
 // create a tbody variable to get a handle on the html element
-var tbody = d3.select('tbody');
+const tbody = d3.select('tbody');
 
 // function to add data to the html table
-var fullTable = tableData.forEach(report => {
-    var row = tbody.append('tr');
-    Object.values(report).forEach(info => {
-        var cell = row.append('td');
-        cell.text(info);
-    })
-});
+function init(){
+    const fullTable = tableData.forEach(report => {
+        let row = tbody.append('tr');
+        Object.values(report).forEach(info => {
+            let cell = row.append('td');
+            cell.text(info);
+        })
+    });
+}
 
 // select the button
-var button = d3.select('#filter-btn');
+const button = d3.select('#filter-btn');
 
 // select the form
-var form = d3.select('form');
+const form = d3.select('form');
 
 // create an event handler for clicking the button or pressing the enter key
 button.on('click', runEnter);
@@ -33,35 +35,33 @@ function runEnter () {
     d3.event.preventDefault();
 
     // select the input date and get the raw html node
-    var inputDate = d3.select('#datetime');
+    let inputDate = d3.select('#datetime');
 
     // get the value property of the input date
-    var inputValue = inputDate.property('value');
+    let inputValue = inputDate.property('value');
 
-    var dates = tableData.map(date => date.datetime);
+    const dates = tableData.map(date => date.datetime);
     if (dates.includes(inputValue)) {
 
     // use the form input to filter data by date
-    var sighting = tableData.filter(report => report.datetime === inputValue);
+    let sighting = tableData.filter(report => report.datetime === inputValue);
 
     sighting.forEach(report => {
-        var row = tbody.append('tr');
+        let row = tbody.append('tr');
         Object.values(report).forEach(info => {
-            var cell = row.append('td');
+            let cell = row.append('td');
             cell.text(info);
         })
     });
+    }
+    else if (inputValue == '') {
+        init();
     }
     else{
         // display a warning message
         window.alert(`Sorry there are no report available for the date provided.`);
-        // function to add data to the html table
-        var fullTable = tableData.forEach(report => {
-        var row = tbody.append('tr');
-        Object.values(report).forEach(info => {
-            var cell = row.append('td');
-            cell.text(info);
-        })
-    });
+        init();
     }
 }; 
+
+init();
